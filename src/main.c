c08fca53918d460d59f31fe1defe41e27560393d
Henry Fillvock <hfillvock@gmail.com>
2025-05-20 22:57:14 +0000

    modify ascii display to log form

--- src/main.c
+++ src/main.c
@@ -9,9 +9,10 @@
 #define ADS1115_ADDR 0x48       // Endereço padrão do ADS1115 no barramento I2C
 #define CONVERSION_DELAY 4000   // 4ms para conversão a 250 SPS
 #define NUM_SAMPLES 4           // Quantidade de amostragens
-#define BAR_WIDTH 20            // Dimensão da barra visual
+#define BAR_WIDTH 128           // Dimensão da barra visual
 #define MAX_RMS 0.707f          // Nível RMS de referência para 0 dBFS
 #define DC_OFFSET 1.25f         // Offset de tensão do MAX9814
+#define MIN_NORMALIZED 0.001f   // Valor mínimo de escala log, evitando log(0)
 
 // Configuração do ADS1115:
 // - Bit 15: Iniciar conversão single-shot (1)
@@ -78,12 +79,15 @@ int main() {
         float dbfs = 20.0f * log10((rms > 0.0001f ? rms : 0.0001f) / MAX_RMS);
         
         // Calcula o comprimento da barra e a imprime com os valores de RMS e dBFS
-        int barLength = (int)(normalized * BAR_WIDTH);
+        float norm_clamped = (normalized < MIN_NORMALIZED) ? MIN_NORMALIZED : normalized;
+        int barLength = (int)((log10f(norm_clamped) - log10f(MIN_NORMALIZED)) / (0 - log10f(MIN_NORMALIZED)) * BAR_WIDTH);
+        if (barLength < 0) barLength = 0;
+        if (barLength > BAR_WIDTH) barLength = BAR_WIDTH;
 
         printf("Volume: ");
         for (int i = 0; i < BAR_WIDTH; i++) {
             if (i < barLength)
-                printf("#");
+                printf("█");
             else
                 printf(" ");
         }
